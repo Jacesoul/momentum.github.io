301c79bd7b290bbc844452a0fb344621c3a73762
Jace <jace@Jaceui-iMac.local>
2021-11-22 21:55:59 +0900

    onclick, onmouseenter, onmouseleave

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const title = document.querySelector("div.hello:first-child h1"); // CSS selector
 
+console.dir(title);
+
 function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
 // handleTitleClick 자바스크립트에게 실행할수 있도록 ()를 제외하고 전달
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
