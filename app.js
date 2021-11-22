058e7712bddb63b232ba95fcddeb49e9afada67a
Jace <jace@Jaceui-iMac.local>
2021-11-22 23:03:57 +0900

    loginInput.value

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1"); // CSS selector
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+  console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-// handleTitleClick 자바스크립트에게 실행할수 있도록 ()를 제외하고 전달
+loginButton.addEventListener("click", onLoginBtnClick);
