f3aaf460c4f671c1e7cfb18b05913ae7130f9dd3
Jace <jace@Jaceui-iMac.local>
2021-11-22 22:11:44 +0900

    resize, copy, offline, online

--- app.js
+++ app.js
@@ -14,7 +14,28 @@ function handleMouseLeave() {
   title.innerText = "Mouse is gone!";
 }
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("Wifi is back!");
+}
+
 title.addEventListener("click", handleTitleClick);
 // handleTitleClick 자바스크립트에게 실행할수 있도록 ()를 제외하고 전달
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
