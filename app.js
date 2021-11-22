df89e947144ddb0de8df194a5b0847c80a40eed3
Jacesoul <seolju90@gmail.com>
2021-11-22 18:27:18 +0900

    querySelector

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const hellos = document.getElementsByClassName("hello");
+const title = document.querySelector(".hello h1:first-child"); // CSS selector
+const title = document.querySelector("#hello"); // CSS selector
+const title = document.getElementById("hello"); // 위의 코드와 동일한 의미이다
 
-console.log(hellos);
+console.log(title);
