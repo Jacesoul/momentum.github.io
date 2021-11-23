99b0973c51328df1f54f8dfad13e9ccea40cc63e
Jacesoul <seolju90@gmail.com>
2021-11-23 15:15:43 +0900

    Loading Username

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저가 기본적으로 작동하는것을 막는다.
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerHTML = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerHTML = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-/*
-addEventListener가 handleLinkClick 함수를 실행할때 하는 일 
-아래와 같이 handleLinkClick함수를 실행시키는 동시에 
-그 함수 첫번째 인자로 이벤트가 일어나는 정보의 object를 넣어준다. 
-handleLinkClick({information about the event that just happened})
-*/
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
